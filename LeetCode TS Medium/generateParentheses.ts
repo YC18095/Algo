c7a67ca4ad31cb32c3345be517fb3b931deea115
YC18095 <79787621+YC18095@users.noreply.github.com>
2021-08-17 15:51:34 +0700

    Create generateParentheses.ts

---
+++ LeetCode TS Medium/generateParentheses.ts
@@ -0,0 +1,32 @@
+function generateParenthesis(n: number): string[] {
+
+    let res: string[]
+    let stack: string[]
+    
+     if(n === 1) {
+        res.push("()")
+        return res
+    }
+
+    function backTracking(open: number, close: number) : string[] {
+        if (close === n && open === n) {
+            res.push(stack.join(''))
+            return
+        }
+        if (close < open) {
+            stack.push(")")
+            backTracking(open, close + 1)
+            stack.pop()
+        }
+        if (open < n) {
+            stack.push("(")
+            backTracking(open + 1, close)
+            stack.pop()
+        }
+
+    }
+
+    backTracking(0, 0)
+
+    return res
+};
