194afa333ddf80b330a4ef793ed2b53ba9309fdd
YC18095 <79787621+YC18095@users.noreply.github.com>
2021-09-15 11:07:01 +0700

    Create jumGameII.js

---
+++ LeetCode JS Medium/jumGameII.js
@@ -0,0 +1,39 @@
+var jump = function (nums) {
+  let i = 0;
+  let result = 0;
+  let max = 0;
+  if (nums.length === 1) return 0;
+  if (nums.length === 2) return 1;
+  while (true) {
+    if (i < nums.length - 1) {
+      result = result + 1;
+      if (nums[i] >= nums.length - 1) break;
+      let range = i + nums[i];
+      let iTemp = i + 1;
+      while (iTemp <= range) {
+        let valueTemp = iTemp + nums[iTemp];
+        if (max < valueTemp) {
+          max = valueTemp;
+          i = iTemp;
+          if (valueTemp >= nums.length - 1) {
+            result = result + 1;
+            i = valueTemp;
+            break;
+          }
+        }
+        iTemp++;
+      }
+    } else {
+      break;
+    }
+  }
+  return result;
+};
+
+// jump([3,5,2,1,4,6,2])
+// jump([2,3,1,1,4])
+// jump([2,2,2,2])
+// jump([2,3,1,1,4])
+jump([2, 3, 1]);
+// jump([3,2,1])
+// jump([1,2,3])
