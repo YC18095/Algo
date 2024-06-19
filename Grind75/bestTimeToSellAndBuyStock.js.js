95c3ad471d2295ff569d00ce7ed21873a2bd0110
Dung Tran Chi (T) <dungtran7@dmchosting.ca>
2024-06-19 11:48:36 +0700

    Update bestTimeToSellAndBuyStock.js.js

--- Grind75/bestTimeToSellAndBuyStock.js.js
+++ Grind75/bestTimeToSellAndBuyStock.js.js
@@ -10,27 +10,39 @@
 //     return profit
 // };
 
-const maxProfit = function(prices) {
-    let profit = 0
-    let i = 0 
-    let j = i + 1
-    while (true) {       
-            if( profit < (prices[j] - prices[i])) {
-            profit = (prices[j] - prices[i])
-            } 
-        j++;
-        if( j >= prices.length ) {
-            i++
-            j = i + 1
-        }
-        if(i == prices.length) {
-            break;
-        } 
-    }
-    return profit
-}
+// const maxProfit = function(prices) {
+//     let profit = 0
+//     let i = 0
+//     let j = i + 1
+//     while (true) {
+//             if( profit < (prices[j] - prices[i])) {
+//             profit = (prices[j] - prices[i])
+//             }
+//         j++;
+//         if( j >= prices.length ) {
+//             i++
+//             j = i + 1
+//         }
+//         if(i == prices.length) {
+//             break;
+//         }
+//     }
+//     return profit
+// }
 
-maxProfit([7,6,4,3,1])
+const maxProfit = (prices) => {
+  let buy_prices = prices[0];
+  let profit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (buy_prices > prices[i]) {
+      buy_prices = prices[i];
+    }
+    profit = Math.max(profit, prices[i] - buy_prices);
+  }
+  return profit;
+};
+
+maxProfit([7, 6, 4, 3, 1]);
 // maxProfit([7,1,5,3,6,4])
 // maxProfit([1,2])
 // maxProfit([2,1,4])
